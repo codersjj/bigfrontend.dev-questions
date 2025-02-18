4fdd31bbc49bfde29bb69b4336ede001d834760e
codersjj <shanewestlife@outlook.com>
2025-02-18 22:46:55 +0800

    feat(JavaScript Coding Questions): 3. implement Array.prototype.flat() Approach 3: iteration

--- JavaScript/3.implement_Array.prototype.flat()/solution.js
+++ JavaScript/3.implement_Array.prototype.flat()/solution.js
@@ -24,25 +24,57 @@
 //   return res
 // }
 
+// /**
+//  * @param { Array } arr
+//  * @param { number } depth
+//  * @returns { Array }
+//  */
+// function flat(arr, depth = 1) {
+//   // recursive solution 2:
+//   // callbackFn is invoked only for array indexes which have assigned values.
+//   // It is not invoked for empty slots in sparse arrays.
+//   // see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce#description
+//   return arr.reduce((res, item) => {
+//     if (Array.isArray(item) && depth > 0) {
+//       res.push(...flat(item, depth - 1))
+//     } else {
+//       res.push(item)
+//     }
+
+//     return res
+//   }, [])
+// }
+
 /**
  * @param { Array } arr
  * @param { number } depth
  * @returns { Array }
  */
 function flat(arr, depth = 1) {
-  // recursive solution 2:
-  // callbackFn is invoked only for array indexes which have assigned values.
-  // It is not invoked for empty slots in sparse arrays.
-  // see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce#description
-  return arr.reduce((res, item) => {
-    if (Array.isArray(item) && depth > 0) {
-      res.push(...flat(item, depth - 1))
+  // iteration solution:
+  const res = []
+  const stack = []
+
+  for (let i = arr.length - 1; i >= 0; i--) {
+    if (Object.hasOwn(arr, i)) {
+      stack.push({ item: arr[i], depth: 0 })
+    }
+  }
+
+  while (stack.length) {
+    const { item, depth: curDepth } = stack.pop()
+    if (Array.isArray(item) && curDepth < depth) {
+      for (let i = item.length - 1; i >= 0; i--) {
+        if (Object.hasOwn(item, i)) {
+          stack.push({ item: item[i], depth: curDepth + 1 })
+        }
+      }
     } else {
       res.push(item)
     }
+  }
 
-    return res
-  }, [])
+  return res
 }
 
 console.log(flat([1,[2],[3,[4]]]))
