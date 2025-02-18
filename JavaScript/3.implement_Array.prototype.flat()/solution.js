e62307442fe67f1c463610b07fde448e8993544a
codersjj <shanewestlife@outlook.com>
2025-02-18 21:33:04 +0800

    feat(JavaScript Coding Questions): 3. implement Array.prototype.flat() Approach 2: recursion using Array.prototype.reduce()

--- JavaScript/3.implement_Array.prototype.flat()/solution.js
+++ JavaScript/3.implement_Array.prototype.flat()/solution.js
@@ -1,27 +1,48 @@
 // This is a JavaScript coding problem from BFE.dev 
+// /**
+//  * @param { Array } arr
+//  * @param { number } depth
+//  * @returns { Array }
+//  */
+// function flat(arr, depth = 1) {
+//   // recursive solution:
+//   const res = []
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (!Object.hasOwn(arr, i)) {
+//       continue
+//     }
+
+//     const item = arr[i]
+//     if (Array.isArray(item) && depth > 0) {
+//       res.push(...flat(item, depth - 1))
+//     } else {
+//       res.push(item)
+//     }
+//   }
+
+//   return res
+// }
+
 /**
  * @param { Array } arr
  * @param { number } depth
  * @returns { Array }
  */
 function flat(arr, depth = 1) {
-  // recursive solution:
-  const res = []
-
-  for (let i = 0; i < arr.length; i++) {
-    if (!Object.hasOwn(arr, i)) {
-      continue
-    }
-
-    const item = arr[i]
+  // recursive solution 2:
+  // callbackFn is invoked only for array indexes which have assigned values.
+  // It is not invoked for empty slots in sparse arrays.
+  // see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce#description
+  return arr.reduce((res, item) => {
     if (Array.isArray(item) && depth > 0) {
       res.push(...flat(item, depth - 1))
     } else {
       res.push(item)
     }
-  }
 
-  return res
+    return res
+  }, [])
 }
 
 console.log(flat([1,[2],[3,[4]]]))
