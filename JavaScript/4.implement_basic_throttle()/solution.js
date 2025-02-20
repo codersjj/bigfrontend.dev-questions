c0fc8bf5dc49e2ecd84240241677f01340f85a11
codersjj <shanewestlife@outlook.com>
2025-02-20 16:47:22 +0800

    feat(JavaScript Coding Questions): 4. implement basic throttle()

---
+++ JavaScript/4.implement_basic_throttle()/solution.js
@@ -0,0 +1,32 @@
+// This is a JavaScript coding problem from BFE.dev 
+
+/**
+ * @param {(...args:any[]) => any} func
+ * @param {number} wait
+ * @returns {(...args:any[]) => any}
+ */
+function throttle(func, wait) {
+  // your code here
+  let timer = null
+  let stashed = null
+
+  const startCooling = () => {
+    timer = setTimeout(() => {
+      if (stashed) {
+        func.apply(stashed[0], stashed[1])
+        stashed = null
+        startCooling()
+      }
+      timer = null
+    }, wait)
+  }
+
+  return function(...args) {
+    if (!timer) {
+      func.apply(this, args)
+      startCooling()
+    } else {
+      stashed = [this, args]
+    }
+  }
+}
