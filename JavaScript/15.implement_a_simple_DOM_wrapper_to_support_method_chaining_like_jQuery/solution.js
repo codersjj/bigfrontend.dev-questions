ef1eb51f89cdf70789adc56009a45ee22abe1529
codersjj <shanewestlife@outlook.com>
2025-03-08 13:48:00 +0800

    feat(JavaScript Coding Questions): 15. implement a simple DOM wrapper to support method chaining like jQuery

---
+++ JavaScript/15.implement_a_simple_DOM_wrapper_to_support_method_chaining_like_jQuery/solution.js
@@ -0,0 +1,31 @@
+// /**
+//  * @param {HTMLElement} el - element to be wrapped
+//  */
+// function $(el) {
+//   // your code here
+//   return {
+//     css(propertyName, value) {
+//       el.style[propertyName] = value
+//       return this
+//     }
+//   }
+// }
+
+/**
+ * @param {HTMLElement} el - element to be wrapped
+ */
+function $(el) {
+  // your code here
+  return new Wrapper(el)
+}
+
+class Wrapper {
+  constructor(el) {
+    this.el = el
+  }
+
+  css(propertyName, value) {
+    this.el.style[propertyName] = value
+    return this
+  }
+}
