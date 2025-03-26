92404d116c63af20fec979011a02ca7299891318
codersjj <shanewestlife@outlook.com>
2025-03-26 09:24:35 +0800

    feat(JavaScript Coding Questions): 26. implement Object.assign() update

--- JavaScript/26.implement_Object.assign()/solution.js
+++ JavaScript/26.implement_Object.assign()/solution.js
@@ -20,9 +20,11 @@ function objectAssign(target, ...sources) {
   // if (typeof target === 'number') target = new Number(target)
   // if (typeof target === 'boolean') target = new Boolean(target)
 
-  if (['string', 'number', 'boolean'].includes(typeof target)) {
-    target = Object(target)
-  }
+  // if (['string', 'number', 'boolean'].includes(typeof target)) {
+  //   target = Object(target)
+  // }
+  
+  if (typeof target !== 'object') target = Object(target)
 
   for (const source of sources) {
     if (source === null || source === undefined) continue
@@ -34,7 +36,7 @@ function objectAssign(target, ...sources) {
     ]
 
     keys.forEach(key => {
-      if (readOnlyProps.includes(key)) throw new Error(`"${key}" is read-only`)
+      if (readOnlyProps.includes(key)) throw new Error(`Cannot assign to read only property '${key}' of object`)
       target[key] = source[key]
     })
   }
@@ -53,9 +55,10 @@ function objectAssign(target, ...sources) {
   // your code here
   if (target === undefined || target === null) throw new Error('Cannot convert undefined or null to object')
   
-  if (['string', 'number', 'boolean'].includes(typeof target)) {
-    target = Object(target)
-  }
+  // if (['string', 'number', 'boolean', 'symbol', 'bigint'].includes(typeof target)) {
+  //   target = Object(target)
+  // }
+  if (typeof target !== 'object') target = Object(target)
 
   for (const source of sources) {
     if (source === null || source === undefined) continue
@@ -68,7 +71,7 @@ function objectAssign(target, ...sources) {
 
     for (const key of keys) {
       if (!Reflect.set(target, key, source[key])) {
-        throw new Error(`"${key}" is read-only`)
+        throw new Error(`Cannot assign to read only property '${key}' of object`)
       }
     }
   }
@@ -81,6 +84,15 @@ function objectAssign(target, ...sources) {
 
 console.log(Object.assign(0, {a: 2342}))
 console.log(objectAssign(0, {a: 2342}))
+
+console.log(Object.assign({}, 'abc'))
+console.log(objectAssign({}, 'abc'))
+
+console.log(Object.assign(Symbol('a'), { height: 20 }))
+console.log(objectAssign(Symbol('a'), { height: 20 }))
+console.log(Object.assign(2n, { height: 20 }))
+console.log(objectAssign(2n, { height: 20 }))
+
 const sourceObj = {}
 const s1 = Symbol('s1')
 Object.defineProperty(sourceObj, s1, {
@@ -95,3 +107,15 @@ const target = Object.defineProperty({}, 'foo', {
   writable: false
 }); // target.foo is a read-only property
 target.foo = 2333
+// console.log(Object.assign(target, { bar: 666, foo: 999 }))
+// console.log(objectAssign(target, { bar: 666, foo: 999 }))
+
+const target2 = Object.defineProperty({}, 'foo', {
+  set(val) {
+    console.log('set foo~')
+    throw new Error('Setting foo was rejected')
+  }
+})
+// target2.foo = '233'
+// console.log(Object.assign(target2, { bar: 666, foo: 999 }))
+// console.log(objectAssign(target2, { bar: 666, foo: 999 }))
